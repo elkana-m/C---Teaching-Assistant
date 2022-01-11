613fd0ead59c13e0bce17dcee3e70f5aa16b835f
elkana-m <munganga.elkana@yahoo.fr>
2022-01-10 18:58:53 -0600

    Last commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C___Teaching_Assistant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
